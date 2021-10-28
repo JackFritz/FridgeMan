054082e9c33bca00d0540e39237aa3d85645e56d
JackFritz <jfritz@quinnipiac.edu>
2021-10-28 15:50:15 -0400

    Open World Stuff
    Changed environment and almost fixed movement

--- FridgeMan/Assets/Scripts/OpenWorld/ThirdPersonMovement.cs
+++ FridgeMan/Assets/Scripts/OpenWorld/ThirdPersonMovement.cs
@@ -9,8 +9,11 @@ public class ThirdPersonMovement : MonoBehaviour
 {
     public CharacterController controller;
     public Animator animator;
+    public bool facingRight;
 
     public float speed = 6f;
+    public float horizontal;
+    public float vertical;
 
     // Update is called once per frame
     void Update()
@@ -19,12 +22,25 @@ public class ThirdPersonMovement : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-        animator.SetFloat("Speed", Mathf.Abs(horizontal));
-        
 
         if (direction.magnitude >= 0.1f)
         {
             controller.Move(direction * speed * Time.deltaTime);
         }
+        
+       
+        animator.SetFloat("Speed", Mathf.Abs(horizontal));
+        //animator.SetFloat("Speed", Mathf.Abs(vertical));
+
+
+        //flips the sprite
+        if ((horizontal < 0 && facingRight) || (horizontal > 0 && !facingRight))
+        {
+            facingRight = !facingRight;
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+            
+        }    
     }
 }
