f2880d2da24692d1a520a5d87336a0f83c73ffa9
JackFritz <jfritz@quinnipiac.edu>
2021-11-16 15:28:38 -0500

    PlaytestPrep
    d

---
+++ FridgeMan/Assets/Scripts/OpenWorld/StoryTime.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StoryTime : MonoBehaviour
+{
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "DonutMan")
+        {
+            SceneManager.LoadScene("StoryTime");
+        }
+    }
+}
